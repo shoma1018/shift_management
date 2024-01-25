d9ffc211fa1e58738154feba72f7ebfbeea4eba6
shoma1018 <gori79ty@gmail.com>
2024-01-25 23:57:45 +0900

    テストユーザーデータ挿入

---
+++ database/seeders/MultiAuthUserSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+
+class MultiAuthUserSeeder extends Seeder
+{
+    
+    public function run()
+    {
+        DB::table('multi_auth_users')->insert([
+                'id' => '1',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+         
+         DB::table('multi_auth_users')->insert([
+                'id' => '2',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+         
+         DB::table('multi_auth_users')->insert([
+                'id' => '3',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+         
+         DB::table('multi_auth_users')->insert([
+                'id' => '4',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+         
+         DB::table('multi_auth_users')->insert([
+                'id' => '5',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+    }
+}
