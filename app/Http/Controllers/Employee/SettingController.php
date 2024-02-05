6b5c83bc0abaf7eccf86eb2312d3a936199e29c5
shoma1018 <gori79ty@gmail.com>
2024-02-06 01:13:04 +0900

    ユーザー情報編集機能(従業員)

--- app/Http/Controllers/Employee/SettingController.php
+++ app/Http/Controllers/Employee/SettingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Employee;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Employee;
 use Illuminate\Support\Facades\Auth;
 
 class SettingController extends Controller
@@ -12,5 +13,18 @@ class SettingController extends Controller
     {
         $selected_employee = Auth::guard('employees')->user();
         return view('employee.setting.index')->with(['employee' => $selected_employee]);
+    }
+    
+    public function edit()
+    {
+        $selected_employee = Auth::guard('employees')->user();
+        return view('employee.setting.edit')->with(['employee' => $selected_employee]);
+    }
+    
+    public function update(Request $request, Employee $employee)
+    {
+        $input = $request['employee'];
+        $employee->fill($input)->save();
+        return redirect('/employee/setting');
     }
 }
