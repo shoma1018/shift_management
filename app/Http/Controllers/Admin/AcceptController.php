711893204abe777a275b6420d7005c9427758f61
shoma1018 <gori79ty@gmail.com>
2024-02-25 16:27:05 +0900

    日付のフォーマット再修正+期間検索機能修正

--- app/Http/Controllers/Admin/AcceptController.php
+++ app/Http/Controllers/Admin/AcceptController.php
@@ -11,19 +11,26 @@ class AcceptController extends Controller
 {
     public function index(Request $request, ShiftApplication $shift_application, AbsenceApplication $absence_application)
     {
-        $from = $request['from'];
-        $until = $request['until'];
-        
         //シフト申請の取得
+        $shift_from = $request['shift_from'];
+        $shift_until = $request['shift_until'];
+        
+        //全件取得
         $selected_shift_applications = $shift_application->get();
-        if (isset($from) && isset($until)) {
-            $selected_shift_applications = $shift_application->whereBetween("date", [$from, $until])->get();
+        //期間検索
+        if (isset($shift_from) && isset($shift_until)) {
+            $selected_shift_applications = $shift_application->whereBetween("created_at", [$shift_from, $shift_until])->get();
         }
         
         //欠勤申請の取得
+        $absence_from = $request['absence_from'];
+        $absence_until = $request['absence_until'];
+        
+        //全件取得
         $selected_absence_applications = $absence_application->get();
-        if (isset($from) && isset($until)) {
-            $selected_absence_applications = $absence_application->whereBetween("date", [$from, $until])->get();
+        //期間検索
+        if (isset($absence_from) && isset($absence_until)) {
+            $selected_absence_applications = $absence_application->whereBetween("created_at", [$absence_from, $absence_until])->get();
         }
         
         return view('admin.accept.index')->with(['shift_applications' => $selected_shift_applications, 'absence_applications' => $selected_absence_applications]);
